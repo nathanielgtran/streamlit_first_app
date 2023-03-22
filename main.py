28bb1e7ffd018c3c8b02270d215699e7d0574ceb
Nathaniel Tran <53902114+nathanielgtran@users.noreply.github.com>
2023-03-22 03:20:56 +0000

    add a df

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Our first Streamlit App')
 st.subheader('intro to streamlit and automate everything with python')
+
+data{
+  'Series_1':[1,2,4,5,9],
+  'Series_2':[11,22,66,144,255,]
+}
+
+df = pd.Dataframe(data)
+
+
+
 st.write(''' this us our first web app
 
 wow very nice!
@@ -15,3 +26,5 @@ wow very nice!
 
 
 ''')
+
+st.write(df)
